4697bf832bb206185c966e8d9c3d276fbef41f3a
Mikita Shah <mshah2511@gmail.com>
2019-05-03 22:21:51 +1000

    Updating methods in page classes for login scenario

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,31 +1,67 @@
 package pages;
 
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
 
 public class HomePage extends BasePage {
 
     @FindBy(name = "username")
     private WebElement usernameField;
 
-    @FindBy(id = "password")
+    @FindBy(css = "input[data-ref='home-login-password']")
     private WebElement passwordField;
 
     @FindBy(css = "button[data-ref='home-login-button']")
     private WebElement loginButton;
-//
-//    private static final String PAGE_TITLE = "Office Supplies, Stationery & Office Furniture at the Lowest Prices | Officeworks";
-//    private static final String PAGE_URL = "https://www.officeworks.com.au/";
 
-    public HomePage(WebDriver driver) {
-        PageFactory.initElements(driver,this);
+    @FindBy(linkText = "Log out")
+    private WebElement logoutLink;
+
+    @FindBy(linkText = "My Account")
+    private WebElement myAccountButton;
+
+    private WebDriverWait wait = new WebDriverWait(driver, 5);
+
+    public HomePage() {
+        PageFactory.initElements(driver, this);
     }
 
     public void loginUser(String userName, String password) {
+        wait.until(ExpectedConditions.visibilityOf(usernameField));
         usernameField.sendKeys(userName);
         passwordField.sendKeys(password);
         loginButton.click();
+    }
+
+    public boolean isLoginSuccessful() {
+        ArrayList<WebElement> accountButtons = new ArrayList<WebElement>();
+        accountButtons.add(logoutLink);
+        accountButtons.add(myAccountButton);
+        wait.until(ExpectedConditions.visibilityOfAllElements(accountButtons));
+
+        return logoutLink.isDisplayed() && myAccountButton.isDisplayed();
+    }
+
+    public void logoffUser() {
+        try {
+            wait.until(ExpectedConditions.visibilityOf(logoutLink));
+            if (logoutLink.isDisplayed()) {
+                logoutLink.click();
+            }
+        } catch (NoSuchElementException e) {
+            //Do nothing
+        }
+    }
+
+    public void navigateToHomePage() {
+        if (!driver.getCurrentUrl().equals("https://www.officeworks.com.au")) {
+            driver.get("https://www.officeworks.com.au");
+        }
     }
 }
--- src/test/java/pages/TopNavigationMenu.java
+++ src/test/java/pages/TopNavigationMenu.java
@@ -1,7 +1,22 @@
 package pages;
 
-/**
- * Created by mikita.shah on 1/05/2019.
- */
-public class TopNavigationMenu {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class TopNavigationMenu extends BasePage {
+
+    @FindBy(css = "div[data-ref='navLink-login']")
+    private WebElement loginOption;
+
+    private Actions actions = new Actions(driver);
+
+    public TopNavigationMenu() {
+        PageFactory.initElements(driver, this);
+    }
+
+    public void clickOnLoginOption() {
+        actions.moveToElement(loginOption).perform();
+    }
 }
