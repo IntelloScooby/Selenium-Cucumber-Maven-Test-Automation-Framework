c21621a32100180d42e5aa17fb20080409d77dcb
Mikita Shah <mshah2511@gmail.com>
2019-05-03 22:24:49 +1000

    Updating stepDefs

--- src/test/java/steps/StepDefs.java
+++ src/test/java/steps/StepDefs.java
@@ -1,56 +1,70 @@
 package steps;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import pages.BasePage;
 import pages.HomePage;
+import pages.MiniLoginDropdown;
+import pages.TopNavigationMenu;
+import utils.TestDataReader;
+import utils.TestHelper;
 
-import static utils.Constants.HOME_PAGE_TITLE;
+import java.util.HashMap;
 
-public class StepDefs {
+import static utils.Constants.*;
 
-    WebDriver webDriver = BasePage.driver;
-    HomePage homePage = new HomePage(webDriver);
+public class StepDefs extends BasePage {
+
+    private HomePage homePage = new HomePage();
+    private TopNavigationMenu topNavigationMenu = new TopNavigationMenu();
+    private MiniLoginDropdown miniLoginDropdown = new MiniLoginDropdown();
+    private TestHelper helper = new TestHelper();
+    private TestDataReader dataReader = new TestDataReader();
 
     @Then("^the user is able to login successfully$")
-    public void theUserIsAbleToLoginSuccessfully() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void theUserIsAbleToLoginSuccessfully() {
+        Assert.assertTrue(homePage.isLoginSuccessful(), "Unable to successfully login");
     }
 
     @Given("^the home page is displayed$")
     public void theHomePageIsDisplayed() {
-        String actualPageTitle = webDriver.getTitle();
+        try {
+            helper.waitForPageToLoad(driver);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        String actualPageTitle = driver.getTitle();
         Assert.assertEquals(actualPageTitle, HOME_PAGE_TITLE, "Home page title is not displayed correctly");
     }
 
     @When("^the user tries to login with valid credentials from the home page$")
     public void theUserTriesToLoginWithValidCredentialsFromTheHomePage() {
-        String username = "testmail@grr.la";
-        String password = "Welcome@123";
+        HashMap<String, String> loginDetails = dataReader.getLoginDetails(VALID_USER);
+        String username = loginDetails.get(USERNAME);
+        String password = loginDetails.get(PASSWORD);
         homePage.loginUser(username, password);
     }
 
-    @And("^the user hover on the login menu option$")
-    public void theUserHoverOnTheLoginMenuOption() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    @And("^the user clicks on the login menu option$")
+    public void theUserClicksOnTheLoginMenuOption() {
+        topNavigationMenu.clickOnLoginOption();
     }
 
     @And("^the mini login dropdown is displayed$")
-    public void theMiniLoginDropdownIsDisplayed() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void theMiniLoginDropdownIsDisplayed() {
+        Assert.assertTrue(miniLoginDropdown.isLoginDropdownDisplayed(), "Mini login dropdown is not displayed");
     }
 
     @When("^the user tries to login with valid credentials from the mini login dropdown$")
-    public void theUserTriesToLoginWithValidCredentialsFromTheMiniLoginDropdown() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void theUserTriesToLoginWithValidCredentialsFromTheMiniLoginDropdown() {
+        HashMap<String, String> loginDetails = dataReader.getLoginDetails(VALID_USER);
+        String username = loginDetails.get(USERNAME);
+        String password = loginDetails.get(PASSWORD);
+        miniLoginDropdown.loginUser(username, password);
     }
 }
--- src/test/resources/features/Login.feature
+++ src/test/resources/features/Login.feature
@@ -3,11 +3,11 @@ Feature: Validate login feature for the website
   Scenario: Validate user is able to login successfully from the Home page
     Given the home page is displayed
     When the user tries to login with valid credentials from the home page
-#    Then the user is able to login successfully
+    Then the user is able to login successfully
 
-#  Scenario: Validate user is able to login successfully from the mini login dropdown
-#    Given the home page is displayed
-#    And the user hover on the login menu option
-#    And the mini login dropdown is displayed
-#    When the user tries to login with valid credentials from the mini login dropdown
-#    Then the user is able to login successfully
+  Scenario: Validate user is able to login successfully from the mini login dropdown
+    Given the home page is displayed
+    And the user clicks on the login menu option
+    And the mini login dropdown is displayed
+    When the user tries to login with valid credentials from the mini login dropdown
+    Then the user is able to login successfully
